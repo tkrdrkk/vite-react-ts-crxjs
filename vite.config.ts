82471d61dd08fdb0ee1be868595e3b150fb5a4a4
tokyorush <tokyorush@icloud.com>
2025-08-13 21:18:47 +0900

    crxjs manual setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.config.ts'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
 })
